4d18ad10629e64d7f1d0062f5a0da844dcd52319
Guganesan.E <guganesh36@gmail.com>
2016-06-29 13:19:47 +0530

    Update timedifference.c

--- timedifference.c
+++ timedifference.c
@@ -4,12 +4,12 @@ void main()
 {
  int hr1,min1,hr2,min2,p,q,r,s,t;
  clrscr();
- printf("\nFirst time: ");
- scanf("\n%d\n%d",&hr1,&min1);
+ printf("First time: ");
+ scanf("%d\n%d",&hr1,&min1);
  p=hr1*60;
  t=min1+p;
- printf("\nSecond time: ");
- scanf("\n%d\n%d",&hr2,&min2);
+ printf("Second time: ");
+ scanf("%d\n%d",&hr2,&min2);
  q=hr2*60;
  s=min2+q;
  r=s-t;
