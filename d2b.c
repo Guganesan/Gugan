bdb6aa92a2b199372aa31713aae9e71f20c1a4fc
Guganesan.E <guganesh36@gmail.com>
2016-07-04 09:55:47 +0530

    Create d2b.c

---
+++ d2b.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+void main(void) {
+int c,bin[100],i=0,q;
+clrscr();
+printf("Enter binary number:");
+scanf("%d",&c);
+q=c;
+while(q!=0)
+{
+	bin[i++]=q%2;
+	q=q/2;
+}
+while(i--)
+printf("%d",bin[i]);
+getch();
+}
