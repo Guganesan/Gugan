08f7d936866eb5f58bf117b96c1a17da4cb6ae32
Guganesan.E <guganesh36@gmail.com>
2016-07-08 10:31:15 +0530

    Create natural.c

---
+++ natural.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,i,b=0;
+clrscr();
+printf("Enter the number: ");
+scanf("%d",&a);
+for(i=1;i<=a;i++)
+{
+b=b+i;
+}
+printf("\nthe sum of natural numbers is %d",b);
+
+getch();}
