1ab4d0e1517ce002c2fb2bb3d87c7a8846414a1d
Guganesan.E <guganesh36@gmail.com>
2016-07-09 09:36:27 +0530

    Create naturalnumbers.c

---
+++ naturalnumbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i,a;
+printf("Enter no of times to repeat: ");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+a=n*(n+1)/2;
+
+}
+printf("%d",a);
+getch();
+}
